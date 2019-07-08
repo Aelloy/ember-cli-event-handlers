d4e3b02eb43999a5f72a9e819eedbac4038e53d7
ryedeer <bisyarin@mail.ru>
2019-07-08 14:01:35 +0300

    Updated acceptance test (all tests now pass)

--- tests/acceptance/main-test.js
+++ tests/acceptance/main-test.js
@@ -1,56 +1,42 @@
-import { test } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { visit, click, find, triggerEvent } from '@ember/test-helpers';
 
-moduleForAcceptance('Acceptance | Main');
+module('Acceptance | Main', function(hooks) {
+  setupApplicationTest(hooks);
 
-test('it works (run with autoApply: true)', function(assert) {
-  assert.expect(10);
-  visit('/');
+  test('it works (run with autoApply: true)', async function(assert) {
+    assert.expect(10);
+    await visit('/');
 
-  click('button.button');
-  andThen(() => {
-    assert.equal(find(".clicked").text(), "0", "Handling is off");
-  });
+    await click('button.button');
+    assert.equal(find(".clicked").textContent, "0", "Handling is off");
 
-  click('button.handling');
-  click('button.button');
-  andThen(() => {
-    assert.equal(find(".clicked").text(), "1", "Handling is on, and we're counting");
-  });
+    await click('button.handling');
+    await click('button.button');
+    assert.equal(find(".clicked").textContent, "1", "Handling is on, and we're counting");
 
-  click('button.handling');
-  click('button.button');
-  andThen(() => {
-    assert.equal(find(".clicked").text(), "1", "Handling is off again, no counting");
-  });
+    await click('button.handling');
+    await click('button.button');
+    assert.equal(find(".clicked").textContent, "1", "Handling is off again, no counting");
 
-  click('button.resize');
-  andThen(() => {
-    assert.equal(find(".resized").text(), "1", "Handling of resizes automatic, counting");
-  });
+    await click('button.resize');
+    assert.equal(find(".resized").textContent, "1", "Handling of resizes automatic, counting");
 
-  click('button.show');
-  andThen(() => {
-    assert.notOk(find("button.button").length, "component disappears");
-  });
+    await click('button.show');
+    assert.notOk(find("button.button"), "component disappears");
 
-  click('button.resize');
-  andThen(() => {
-    assert.equal(find(".resized").text(), "1", "Handler was off, when component disappeared, no counting");
-  });
+    await click('button.resize');
+    assert.equal(find(".resized").textContent, "1", "Handler was off, when component disappeared, no counting");
 
-  click('button.show');
-  andThen(() => {
-    assert.equal(find(".scrolled").text(), "0", "Initially scrolled 0 times");
-    assert.equal(find(".visibility").text(), "", "visibilitychange never triggered");
-  });
+    await click('button.show');
+    assert.equal(find(".scrolled").textContent, "0", "Initially scrolled 0 times");
+    assert.equal(find(".visibility").textContent, "", "visibilitychange never triggered");
 
-  triggerEvent(".scrollable", "scroll");
-  andThen(() => {
-    assert.equal(find(".scrolled").text(), "1", "Scrolled 1 time");
-  });
-  triggerEvent(document, "visibilitychange");
-  andThen(() => {
-    assert.equal(find(".visibility").text(), "visible", "Becomes visible (kinda)");
+    await triggerEvent(".scrollable", "scroll");
+    assert.equal(find(".scrolled").textContent, "1", "Scrolled 1 time");
+
+    await triggerEvent(document, "visibilitychange");
+    assert.equal(find(".visibility").textContent, "visible", "Becomes visible (kinda)");
   });
 });
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -6,12 +6,3 @@ import { start } from 'ember-qunit';
 setApplication(Application.create(config.APP));
 
 start();
-
-// import resolver from './helpers/resolver';
-// import {
-//   setResolver
-// } from '@ember/test-helpers';
-// import { start } from 'ember-cli-qunit';
-
-// setResolver(resolver);
-// start();
